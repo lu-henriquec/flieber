44a451a76b67af07af7250d32be2d399339f59b3
lu-henriquec <lucaa.henrique.carlos@gmail.com>
2019-11-20 15:58:00 -0300

    px to rem / ajustes container

--- wp-content/themes/flieber/index.php
+++ wp-content/themes/flieber/index.php
@@ -127,14 +127,18 @@
       <p class='text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
       <div class="content">
         <div class="box">
-          <img src="<?php bloginfo('template_url'); ?>/images/advantages/Icon_Increase_Your_Sales.svg" width="111" height="94" />
+          <div class="box__image">
+            <img src="<?php bloginfo('template_url'); ?>/images/advantages/Icon_Increase_Your_Sales.svg" width="111" height="94" />
+          </div>
           <h3>Increase Your Sales</h3>
           <p>
             Make sure you always have your products available for sale. No more stock-outs.
           <p>
         </div>
         <div class="box">
-          <img src="<?php bloginfo('template_url'); ?>/images/advantages/icon_Free-up_Trapped_Capital.svg" width="128" height="80" />
+          <div class="box__image">
+            <img src="<?php bloginfo('template_url'); ?>/images/advantages/icon_Free-up_Trapped_Capital.svg" width="128" height="80" />
+          </div>
           <h3>Free-up Trapped Capital</h3>
           <p>
             Allocate capital efficiently
@@ -143,28 +147,36 @@
           <p>
         </div>
         <div class="box">
-          <img src="<?php bloginfo('template_url'); ?>/images/advantages/icon_Increase_Your_Margin.svg" width="118" height="108" />
+          <div class="box__image">
+            <img src="<?php bloginfo('template_url'); ?>/images/advantages/icon_Increase_Your_Margin.svg" width="118" height="108" />
+          </div>
           <h3>Increase Your Margin</h3>
           <p>
             Prioritize the right products and sell the ideal quantities to optimize for margin.
           <p>
         </div>
         <div class="box">
-          <img src="<?php bloginfo('template_url'); ?>/images/advantages/icon_Maximize_Your_Working_Capital.svg" width="111" height="111" />
+          <div class="box__image">
+            <img src="<?php bloginfo('template_url'); ?>/images/advantages/icon_Maximize_Your_Working_Capital.svg" width="111" height="111" />
+          </div>
           <h3>Maximize Your Working Capital</h3>
           <p>
             Reduce the capital cycle of your business by streamlining your purchase-to-sales process.
           <p>
         </div>
         <div class="box">
-          <img src="<?php bloginfo('template_url'); ?>/images/advantages/icon_Reduce_Bad_Reviews.svg" width="146" height="78" />
+          <div class="box__image">
+            <img src="<?php bloginfo('template_url'); ?>/images/advantages/icon_Reduce_Bad_Reviews.svg" width="146" height="78" />
+          </div>
           <h3>Reduce Bad Reviews</h3>
           <p>
             Assure the quality level of your products so that bad reviews don't hurt your sales anymore.
           <p>
         </div>
         <div class="box">
-          <img src="<?php bloginfo('template_url'); ?>/images/advantages/icon_Eliminate_Your_Headaches.svg" width="100" height="101" />
+          <div class="box__image">
+            <img src="<?php bloginfo('template_url'); ?>/images/advantages/icon_Eliminate_Your_Headaches.svg" width="100" height="101" />
+          </div>
           <h3>Eliminate Your Headaches</h3>
           <p>
             Leverage on our system and our team to reduce the complexities of your supply-chain.
